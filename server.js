22b48535cb6b1d48eeb1e6680917422d735521b4
Mohamed Hamza <mohamedhamza@aucegypt.edu>
2021-05-23 17:21:20 +0200

    modified config for deployment

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
     });
 
-const server = http.listen(3000, () => {
+const server = http.listen(process.env.PORT || 3000, () => {
     console.log('Server is listening on port: ', server.address().port)
 });
 
