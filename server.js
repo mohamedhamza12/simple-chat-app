003a30972addb00f40d3403c843aca5255ac71ff
Mohamed Hamza <mohamedhamza@aucegypt.edu>
2021-05-25 21:13:59 +0200

    call next in access-control middleware

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ const io = require('socket.io')(http);
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res) => {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+    next();
 });
 
 const dbUrl = config.dbdbConnectionString;
